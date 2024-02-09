351dd3e58aef907d689caba2950a32b5946a35c2
Srajal <srajal534@gmail.com>
2024-02-10 01:23:30 +0530

    cra + tailwind

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="text-3xl font-bold">
+        Jai Jinendra sabko
+      </div>
+    </div>
+  );
+}
+
+export default App;
